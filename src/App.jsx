616048583a65eeeb54af3a15ea07a0d0db451695
Shubham Vyas <your-svvyas96@gmail.com>
2026-04-13 23:52:50 +0530

    resume website

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App(){
+return(
+<div style={{padding:20,fontFamily:'Arial'}}>
+<h1>Shubham Vyas</h1>
+<h2>QA Engineer</h2>
+<a href="/Shubham_Vyas_Resume.pdf" target="_blank">Download Resume</a>
+</div>
+)
+}
